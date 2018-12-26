1583c77ecc69d2701b4e7031ce7462019aba6aeb
lidongjie <dongjie@weico.com>
2018-12-26 15:36:06 +0800

    cocoapods集成SDK

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'Push_WeChat_Alipay' do
+
+pod 'JPush'
+pod 'WechatOpenSDK'
+pod 'AlipaySDK-iOS'
+
+end
---
+++ Push_WeChat_Alipay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Push_WeChat_Alipay
+//
+//  Created by Geraint on 2018/12/26.
+//  Copyright © 2018 kilolumen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
